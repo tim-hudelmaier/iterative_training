f55998a3ba1a813e7f29ce0ad1c6a0c4034a8b5d
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-11-18 15:46:28 +0100

    fix gen and pickle samples to make test pass

--- iter_helpers/iter_helpers.py
+++ iter_helpers/iter_helpers.py
@@ -2,6 +2,7 @@ from itertools import permutations
 import hashlib
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 
@@ -14,8 +15,15 @@ def get_idx_md5(id_list: list, sort_ids=True):
     Returns:
         md5 hash of all spectrum ids
     """
-    all_ids_str = "".join(sorted(id_list.copy())) if sort_ids else "".join(
-        id_list.copy())
+    if isinstance(id_list, str):
+        return hashlib.md5(id_list.encode()).hexdigest()
+    elif isinstance(id_list, pd.Series):
+        id_list = id_list.unique().tolist()
+    else:
+        ValueError(f"id_list must be a list or pd.Series, you provided {type(id_list)}")
+    all_ids_str = (
+        "".join(sorted(id_list.copy())) if sort_ids else "".join(id_list.copy())
+    )
     return hashlib.md5(all_ids_str.encode()).hexdigest()
 
 
@@ -67,26 +75,30 @@ def generate_eval_df(n_iterations: int, evals_dir: Path):
 
 
 def generate_and_pickle_samples(
-        df: pd.DataFrame,
-        sample_group_col: str,
-        n_samples: int,
-        sample_dir: Path,
-        file_extension: str
+    df: pd.DataFrame,
+    sample_group_col: str,
+    n_samples: int,
+    sample_dir: Path,
+    file_extension: str,
 ):
     """Generate and pickle samples from input df."""
-    available_samples = df[sample_group_col].unique()
+    available_samples = pd.Series(df[sample_group_col].unique())
 
     all_samples_md5 = get_idx_md5(available_samples, sort_ids=True)
 
-    for samples in available_samples.sample(frac=1 / n_samples):
+    shuffled_samples = available_samples.sample(frac=1).reset_index(drop=True)
+
+    split_indices = np.array_split(shuffled_samples, n_samples)
+    for samples in split_indices:
         # md5 hash all sample spectrum ids
         used_samples_md5 = get_idx_md5(samples, sort_ids=True)
 
         # save as pkl
+        if isinstance(samples, str):
+            samples = [samples]
+
         sample_df = df[df[sample_group_col].isin(samples)].copy()
-        sample_df.to_pickle(
-            sample_dir / f"sample_{used_samples_md5}.{file_extension}"
-        )
+        sample_df.to_pickle(sample_dir / f"sample_{used_samples_md5}.{file_extension}")
 
     return all_samples_md5
 
@@ -95,11 +107,13 @@ def generate_next_train_run(sample_files):
     train_combinations = permutations(sample_files, len(sample_files))
 
     for train_combination in train_combinations:
-        train_path_md5 = get_idx_md5([str(f) for f in train_combination],
-                                     sort_ids=False)
+        train_path_md5 = get_idx_md5(
+            [str(f) for f in train_combination], sort_ids=False
+        )
 
         for i, train_sample_file in enumerate(train_combination):
-            eval_sample_file = train_combination[i + 1] if i < len(
-                train_combination) - 1 else None
+            eval_sample_file = (
+                train_combination[i + 1] if i < len(train_combination) - 1 else None
+            )
             finished_path = True if i == len(train_combination) - 1 else False
             yield i, train_sample_file, eval_sample_file, train_path_md5, finished_path
