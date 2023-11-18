17816e958b599f9fd0ac31fca4672f8ab28b2ecf
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-11-18 12:36:15 +0100

    restructure and add working tests

--- main.py
+++ main.py
@@ -17,114 +17,19 @@
 - train with xgboost extend, random forest xtend and xgboost prune
 """
 
-from itertools import permutations
-import hashlib
 from pathlib import Path
 
 import pandas as pd
 
-from pf_wrapper import train_run, consolidate_evals
-
-
-def get_idx_md5(id_list: list, sort_ids=True):
-    """Get md5 hash of all spectrum ids.
-
-    Args:
-        sorted (bool): whether to sort spectrum ids before hashing
-
-    Returns:
-        md5 hash of all spectrum ids
-    """
-    all_ids_str = "".join(sorted(id_list.copy())) if sort_ids else "".join(
-        id_list.copy())
-    return hashlib.md5(all_ids_str.encode()).hexdigest()
-
-
-def create_dirs(base_dir: Path) -> dict:
-    """Create necessary directories."""
-    sample_dir = base_dir / "sample_dfs"
-    if not sample_dir.exists():
-        sample_dir.mkdir()
-
-    models_dir = base_dir / "models"
-    if not models_dir.exists():
-        models_dir.mkdir()
-
-    results_dir = base_dir / "results"
-    if not results_dir.exists():
-        results_dir.mkdir()
-
-    evals_dir = base_dir / "evals"
-    if not evals_dir.exists():
-        evals_dir.mkdir()
-
-    return {
-        "sample_dir": sample_dir,
-        "models_dir": models_dir,
-        "results_dir": results_dir,
-        "evals_dir": evals_dir,
-    }
-
-
-def drop_cols(df: pd.DataFrame, col_prefixes: list) -> pd.DataFrame:
-    """Drop columns from dataframe."""
-    cols_to_drop = []
-    for col_prefix in col_prefixes:
-        cols_to_drop += [c for c in df.columns if c.startswith(col_prefix)]
-    df.drop(cols_to_drop, axis=1, inplace=True)
-    return df
-
-
-def generate_eval_df(n_iterations: int, evals_dir: Path):
-    for iteration in range(n_iterations):
-        eval_dfs = [p for p in evals_dir.glob(f"*iteration_{iteration}*")]
-
-        eval_df = pd.concat([pd.read_csv(p) for p in eval_dfs])
-
-        eval_spectrum_ids = eval_df["spectrum_id"].unique()
-        eval_spectrum_ids_md5 = get_idx_md5(eval_spectrum_ids, sort_ids=True)
-
-        yield iteration, eval_df, eval_spectrum_ids_md5
-
-
-def generate_and_pickle_samples(
-        df: pd.DataFrame,
-        sample_group_col: str,
-        n_samples: int,
-        sample_dir: Path,
-        file_extension: str
-):
-    """Generate and pickle samples from input df."""
-    available_samples = df[sample_group_col].unique()
-
-    all_samples_md5 = get_idx_md5(available_samples, sort_ids=True)
-
-    for samples in available_samples.sample(frac=1 / n_samples):
-        # md5 hash all sample spectrum ids
-        used_samples_md5 = get_idx_md5(samples, sort_ids=True)
-
-        # save as pkl
-        sample_df = df[df[sample_group_col].isin(samples)].copy()
-        sample_df.to_pickle(
-            sample_dir / f"sample_{used_samples_md5}.{file_extension}"
-        )
-
-    return all_samples_md5
-
-
-def generate_next_train_run(sample_files):
-    train_combinations = permutations(sample_files, len(sample_files))
-
-    for train_combination in train_combinations:
-        train_path_md5 = get_idx_md5([str(f) for f in train_combination],
-                                     sort_ids=False)
-
-        for i, train_sample_file in enumerate(train_combination):
-            eval_sample_file = train_combination[i + 1] if i < len(
-                train_combination) - 1 else None
-            finished_path = True if i == len(train_combination) - 1 else False
-            yield i, train_sample_file, eval_sample_file, train_path_md5, finished_path
-
+from iter_helpers.pf_wrapper import train_run, consolidate_evals
+from iter_helpers.iter_helpers import (
+    get_idx_md5,
+    create_dirs,
+    drop_cols,
+    generate_eval_df,
+    generate_and_pickle_samples,
+    generate_next_train_run,
+)
 
 if __name__ == "__main__":
     n_samples = 5
@@ -173,7 +78,8 @@ if __name__ == "__main__":
             new_model_md5 = input_spectrum_ids_md5
 
         # check if model has already been trained
-        if (dir_dict["models_dir"] / f"model_{new_model_md5}.{file_extension}").exists():
+        if (dir_dict[
+                "models_dir"] / f"model_{new_model_md5}.{file_extension}").exists():
             continue
 
         model_output_path = dir_dict[
--- tests/test_sample_train_data.py
+++ tests/test_sample_train_data.py
@@ -1,4 +1,23 @@
 import pytest
 
-def test_nothing():
-    assert True
+import pandas as pd
+
+from iter_helpers.iter_helpers import drop_cols
+
+def test_drop_cols():
+    data = {"pre_col1": [1, 2], "pre_col2": [3, 4], "other_col": [5, 6]}
+    df = pd.DataFrame(data)
+
+    prefixes_to_drop = ["pre_"]
+    modified_df = drop_cols(df.copy(), prefixes_to_drop)
+    assert "pre_col1" not in modified_df.columns
+    assert "pre_col2" not in modified_df.columns
+    assert "other_col" in modified_df.columns
+
+    modified_df = drop_cols(df.copy(), [])
+    assert all(col in modified_df.columns for col in df.columns)
+
+    # Test with a prefix not present in the DataFrame
+    prefixes_to_drop = ["nonexistent_prefix_"]
+    modified_df = drop_cols(df.copy(), prefixes_to_drop)
+    assert all(col in modified_df.columns for col in df.columns)
